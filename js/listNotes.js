f340cbb6796b3afe968b70da0ed6623730bb797f
Judith Gull <judith.gull@gmail.com>
2015-05-30 15:01:26 +0200

    add format date function for note template

--- js/listNotes.js
+++ js/listNotes.js
@@ -6,10 +6,26 @@ $(function () {
     var notes = JSON.parse(notesStr);
     console.log("Current notes in session store:", notes);
 
+    moment.locale("de-CH"); //TODO use browser locale
+    Handlebars.registerHelper("formatDate", formatDate);
     var createNotesHtml = Handlebars.compile($("#notes-entry-template").html());
 
     document.getElementById("js-notes-list").innerHTML = createNotesHtml(notes);
+
 });
+
+function formatDate(dateStr) {
+    if (!dateStr) {
+        return "Irgendwann";
+    }
+    else {
+        var m = moment(dateStr, "YYYY-MM-DD");
+        if (m.day() === moment().day() && m.month() === moment().month() && m.year() === moment().year()) {
+            return "Heute";
+        }
+        return moment(dateStr, "YYYY-MM-DD").fromNow();
+    }
+}
 
 /* Makes sure that the local storage contains a notes array and adds some initial data, if no data is available.
 * */
@@ -31,14 +47,14 @@ function getInitialNotes() {
         {
             title: "CAS FEE Selbststudium",
             description: "HTML für die Note App erstellen.\nCSS erstellen für die Note App.\nmore text",
-            dueDate: new Date() + 2,
+            dueDate: moment().add(4, "day").format("YYYY-MM-DD"),
             completionDate: new Date(),
             importance: 2
         },
         {
             title: "Einkaufen",
             description: "Eier\nButter",
-            dueDate: new Date(),
+            dueDate: moment().format("YYYY-MM-DD"),
             completionDate: null,
             importance: 1
         },
