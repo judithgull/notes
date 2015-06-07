89a858f40e8576b532fd575b4bc0bbc3428f466e
corinne roelli <corinne@ginetta.net>
2015-06-07 13:15:07 +0200

    removes comments TODO

--- js/newNote.js
+++ js/newNote.js
@@ -2,15 +2,13 @@ function send() {
     var title = document.getElementById("note-title").value;
     var description = document.getElementById("note-description").value;
     var dueDate = document.getElementById("note-due-date").value;
-    var importance = $(".js-importance-rating").attr("data-radio"); // TODO never nullc
+    var importance = $(".js-importance-rating").attr("data-radio");
     var note = notesStorage.addNote(title, description, dueDate, importance);
 
     //jump to list view
     window.location.replace("/index.html");
     return true;
 }
-
-// TODO: If you click the cancel button it should not submit the form.
 
 function setImportanceValue() {
     var selectedValue = event.toElement.value;
