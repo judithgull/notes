90b498f1c5ec6e912db274cb1f9b4beb691287bb
corinne roelli <corinne@ginetta.net>
2015-06-09 20:43:24 +0200

    sets the attribute checked to the importance element

--- js/newNote.js
+++ js/newNote.js
@@ -26,6 +26,8 @@ $(function(){
         document.getElementById("note-title").value = note.title;
         document.getElementById("note-description").value = note.description;
         document.getElementById("note-due-date").value = dueDateStr;
+        $(".js-importance-rating").attr("data-radio", note.importance);
+        $(".js-importance-rating").find("[value=" + note.importance +"]").prop("checked", true);
     }
 
 });
