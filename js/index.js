2894cb64cab06f34d301ccd55ca51f34efb86258
Judith Gull <judith.gull@gmail.com>
2015-06-13 11:31:51 +0200

    handle markNoteFinished on server

--- js/index.js
+++ js/index.js
@@ -10,7 +10,35 @@ var serve = serveStatic("."),
     server = http.createServer(function (req, res) {
         var done = finalhandler(req, res);
         console.log(req.url);
-        if(req.url.indexOf( "/notes" ) == 0){
+
+        if (req.method === "POST" && req.url === "/markNoteFinished") {
+            var body = '';
+            console.log(body);
+            req.on('data', function (chunk) {
+                body += chunk;
+            });
+
+            req.on('end', function () {
+                try {
+                    var data = JSON.parse(body);
+                    console.log(data);
+                    if (typeof(data.id) != "undefined" && typeof(data.checked) != "undefined") {
+                        storage.markFinished(data.id, data.checked);
+                        return res.end("OK");
+                    } else {
+                        res.statusCode = 400;
+                        return res.end('error: ' + err.message);
+                    }
+
+                } catch (err) {
+                    res.statusCode = 400;
+                    return res.end('error: ' + err.message);
+                }
+            });
+
+
+        }
+        else if (req.url.indexOf("/notes") == 0) {
             res.writeHead(200, {'Content-Type': 'application/json'});
 
             var queryObject = url.parse(req.url,true).query;
--- js/test.js
+++ js/test.js
@@ -1,5 +1,6 @@
 var http = require("http");
 //1337, '127.0.0.1'
+/*
 var req = http.request({
     hostname: "localhost",
     port: 3000,
@@ -13,8 +14,27 @@ var req = http.request({
         console.log('BODY: ' + chunk.length);
     });
 });
+ */
+
+var req = http.request({
+    hostname: "localhost",
+    port: 3000,
+    path: "/markNoteFinished",
+    method: "POST"
+}, function (res) {
+    console.log('STATUS: ' + res.statusCode);
+    console.log('HEADERS: ' + JSON.stringify(res.headers));
+    res.setEncoding('utf8');
+    res.on('data', function (chunk) {
+        console.log('BODY: ' + chunk.length);
+    });
+});
+
+req.write(JSON.stringify({id: 0, checked: true}));
 
 req.on('error', function(e) {
     console.log('problem with request: ' + e.message);
 });
+
+
 req.end();
