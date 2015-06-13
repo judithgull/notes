309d5ab647b43deaa51358a25fe8216e9262efdf
corinne roelli <corinne@ginetta.net>
2015-06-13 12:23:26 +0200

    handle new note on server

--- js/index.js
+++ js/index.js
@@ -6,39 +6,47 @@ var storage = require('./noteStorage.js'),
     http = require("http"),
     finalhandler = require("finalhandler");
 
+
 var serve = serveStatic("."),
     server = http.createServer(function (req, res) {
         var done = finalhandler(req, res);
         console.log(req.url);
-
-        if (req.method === "POST" && req.url === "/markNoteFinished") {
+        if (req.method === "POST"){
             var body = '';
             console.log(body);
             req.on('data', function (chunk) {
                 body += chunk;
             });
 
-            req.on('end', function () {
-                try {
+            if (req.url === "/note") {
+                req.on('end', function () {
                     var data = JSON.parse(body);
-                    console.log(data);
-                    if (typeof(data.id) != "undefined" && typeof(data.checked) != "undefined") {
-                        storage.markFinished(data.id, data.checked);
-                        return res.end("OK");
-                    } else {
+                    storage.addNote(
+                        data.title,
+                        data.description,
+                        data.dueDate,
+                        data.importance);
+                });
+            } else if (req.url === "/markNoteFinished") {
+                req.on('end', function () {
+                    try {
+                        var data = JSON.parse(body);
+                        console.log(data);
+                        if (typeof(data.id) != "undefined" && typeof(data.checked) != "undefined") {
+                            storage.markFinished(data.id, data.checked);
+                            return res.end("OK");
+                        } else {
+                            res.statusCode = 400;
+                            return res.end('error: ' + err.message);
+                        }
+
+                    } catch (err) {
                         res.statusCode = 400;
                         return res.end('error: ' + err.message);
                     }
-
-                } catch (err) {
-                    res.statusCode = 400;
-                    return res.end('error: ' + err.message);
-                }
-            });
-
-
-        }
-        else if (req.url.indexOf("/notes") == 0) {
+                });
+            }
+        } else if (req.url.indexOf("/notes") == 0) {
             res.writeHead(200, {'Content-Type': 'application/json'});
 
             var queryObject = url.parse(req.url,true).query;
--- js/test.js
+++ js/test.js
@@ -1,25 +1,41 @@
 var http = require("http");
 //1337, '127.0.0.1'
 /*
-var req = http.request({
-    hostname: "localhost",
-    port: 3000,
-    path: "/notes?sorting=sort-by-completion&includeFinished=true",
-    method: "GET"
-}, function (res) {
-    console.log('STATUS: ' + res.statusCode);
-    console.log('HEADERS: ' + JSON.stringify(res.headers));
-    res.setEncoding('utf8');
-    res.on('data', function (chunk) {
-        console.log('BODY: ' + chunk.length);
-    });
-});
+ var req = http.request({
+ hostname: "localhost",
+ port: 3000,
+ path: "/notes?sorting=sort-by-completion&includeFinished=true",
+ method: "GET"
+ }, function (res) {
+ console.log('STATUS: ' + res.statusCode);
+ console.log('HEADERS: ' + JSON.stringify(res.headers));
+ res.setEncoding('utf8');
+ res.on('data', function (chunk) {
+ console.log('BODY: ' + chunk.length);
+ });
+ });
+
+
+ var req = http.request({
+ hostname: "localhost",
+ port: 3000,
+ path: "/markNoteFinished",
+ method: "POST"
+ }, function (res) {
+ console.log('STATUS: ' + res.statusCode);
+ console.log('HEADERS: ' + JSON.stringify(res.headers));
+ res.setEncoding('utf8');
+ res.on('data', function (chunk) {
+ console.log('BODY: ' + chunk.length);
+ });
+ });
+
  */
 
 var req = http.request({
     hostname: "localhost",
     port: 3000,
-    path: "/markNoteFinished",
+    path: "/note",
     method: "POST"
 }, function (res) {
     console.log('STATUS: ' + res.statusCode);
@@ -30,9 +46,16 @@ var req = http.request({
     });
 });
 
-req.write(JSON.stringify({id: 0, checked: true}));
+/*
+req.write(JSON.stringify({
+    title: "Einkaufen",
+    description: "Eier\nButter",
+    dueDate: new Date(),
+    importance: 1
+}));*/
 
-req.on('error', function(e) {
+req.write(JSON.stringify({a:"a"}));
+req.on('error', function (e) {
     console.log('problem with request: ' + e.message);
 });
 
