674b3cc42f251a9c564936c7f1f14c9fa42f153b
Judith Gull <judith.gull@gmail.com>
2015-06-15 19:47:39 +0200

    cleanup new note

--- js/newNote.js
+++ js/newNote.js
@@ -1,52 +1,18 @@
-function send() {
-    var title = document.getElementById("note-title").value;
-    var description = document.getElementById("note-description").value;
-    var dueDate = document.getElementById("note-due-date").value;
-    var importance = $(".js-importance-rating").attr("data-radio");
-
-    var id = getNoteId();
-
-    if (id === null) {
-        $.post("/note", JSON.stringify({
-                title: title,
-                description: description,
-                dueDate: dueDate,
-                importance: importance
-            }),
-            function () {
-                //jump to list view
-                window.location.replace("/index.html");
-            });
-    } else {
-        $.ajax({
-            url: "/note",
-            type: "PUT",
-            data: JSON.stringify(
-                {
-                    id: id,
-                    title: title,
-                    description: description,
-                    dueDate: dueDate,
-                    importance: importance
-                })
-        }).done(function () {
-            //jump to list view
-            window.location.replace("/index.html");
-        });
-
-    }
-    return true;
-}
-
-function setImportanceValue() {
-    var selectedValue = event.toElement.value;
-    $(".js-importance-rating").attr("data-radio", selectedValue);
-}
-
+/**
+ * On Load
+ * */
 $(function () {
     var id = getNoteId();
 
     if (id !== null) {
+        load(id);
+    }
+    $("#note").on("submit", submit);
+
+    /**
+     * query note with given id and update html
+     * */
+    function load(id) {
         $.getJSON("/notes?id=" + id, function (note) {
             $("#note-title").val(note.title);
             $("#note-description").val(note.description);
@@ -56,14 +22,41 @@ $(function () {
         });
     }
 
-    document.getElementById("note").addEventListener("submit", send);
+    /**
+     * submit new or update existing note
+     * */
+    function submit() {
+        var data = {
+            id: getNoteId(),
+            title: $("#note-title").val(),
+            description: $("#note-description").val(),
+            dueDate: $("#note-due-date").val(),
+            importance: $(".js-importance-rating").attr("data-radio")
+        };
+
+        var requestSettings = {
+            url: "/note",
+            data: JSON.stringify(data),
+            type: (data.id === null) ? "POST" : "PUT"
+        };
+        $.ajax(requestSettings).done(function () {
+            //jump to list view
+            window.location.replace("/index.html");
+        });
+        return true;
+    }
+
+    function getNoteId() {
+        var idQueryRegex = /[?&]id=([^&#]+)/;
+        var idQueryResult = location.href.match(idQueryRegex);
+        if (idQueryResult !== null) {
+            return idQueryResult[1];
+        }
+        return null;
+    }
 });
 
-function getNoteId() {
-    var idQueryRegex = /[?&]id=([^&#]+)/;
-    var idQueryResult = location.href.match(idQueryRegex);
-    if (idQueryResult !== null) {
-        return idQueryResult[1];
-    }
-    return null;
-};
+function setImportanceValue() {
+    var selectedValue = event.toElement.value;
+    $(".js-importance-rating").attr("data-radio", selectedValue);
+}
