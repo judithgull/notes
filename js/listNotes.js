39e1a4cc5313299b4d2af9c3543af67a677954a0
corinne roelli <corinne@ginetta.net>
2015-06-19 12:53:17 +0200

    Adds new styles and js to show more text

--- js/listNotes.js
+++ js/listNotes.js
@@ -66,7 +66,31 @@ $(function () {
             });
 
 
-
+            var showChar = 200;
+            var ellipsestext = "...";
+            var moretext = "more";
+            var lesstext = "less";
+            $('.more').each(function () {
+                var content = $(this).html();
+                if (content.length > showChar) {
+                    var c = content.substr(0, showChar);
+                    var h = content.substr(showChar - 1, content.length - showChar);
+                    var html = c + '<span class="moreellipses">' + ellipsestext + ' </span><span class="morecontent"><span>' + h + '</span>  <a href="" class="morelink t-link">' + moretext + '</a></span>';
+                    $(this).html(html);
+                }
+            });
+            $(".morelink").click(function () {
+                if ($(this).hasClass("less")) {
+                    $(this).removeClass("less");
+                    $(this).html(moretext);
+                } else {
+                    $(this).addClass("less");
+                    $(this).html(lesstext);
+                }
+                $(this).parent().prev().toggle();
+                $(this).prev().toggle();
+                return false;
+            });
 
         });
 
