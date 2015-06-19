286d08f7c2e695071828c2d6e513a2a7bcc541fa
corinne roelli <corinne@ginetta.net>
2015-06-19 15:59:50 +0200

    Adds query parameter for notes.

--- controller/noteController.js
+++ controller/noteController.js
@@ -1,10 +1,26 @@
-var store = require('../services/noteStorage.js');
+var store = require("../services/noteStorage.js"),
+    url = require("url");
 
 
 module.exports.getNotes = function (req, res) {
     res.format({
         'application/json': function () {
-            res.send(store.getByCompletion(true));
+
+            var queryObject = url.parse(req.url, true).query;
+
+            var includeFinished = queryObject["includeFinished"] === "true";
+            var sorting = queryObject["sorting"];
+            var notes;
+            if (sorting === "sort-by-completion") {
+                notes = store.getByCompletion(includeFinished);
+            } else if (sorting === "sort-by-creation") {
+                notes = store.getByCreation(includeFinished);
+            } else if (sorting === "sort-by-importance") {
+                notes = store.getByImportance(includeFinished);
+            } else {
+
+            }
+            res.send(notes);
         }
     });
 
