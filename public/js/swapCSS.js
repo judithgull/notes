58da92270656ca5560db201772e2e1345ae00e3f
corinne roelli <corinne@ginetta.net>
2015-06-23 20:37:11 +0200

    Separates the function to load and store clicked Stylesheet

--- public/js/swapCSS.js
+++ public/js/swapCSS.js
@@ -2,16 +2,28 @@
 "use strict";
 
 $(function () {
-  document.getElementById("sel-display-style").addEventListener("change", swapStyleSheet);
+  var optionValue = loadCSS();
+  var selStyleEl = document.getElementById("sel-display-style");
+  if (selStyleEl) {
+    $("#sel-display-style").val(optionValue);
+    selStyleEl.addEventListener("change", function(){
+      storeStyleOption();
+      loadCSS();
+    });
+  }
 });
 
 
-
 // changes stylesheet without pagereload.
-function swapStyleSheet(){
+function storeStyleOption(){
   //get the value of options in dropdown.
   var selectorStyle = document.getElementById("sel-display-style");
   var optionValue = selectorStyle.options[selectorStyle.selectedIndex].value;
+  localStorage.setItem("display-style", optionValue);
+}
 
+function loadCSS(){
+  var optionValue = localStorage.getItem("display-style") || "color";
   document.getElementById('js-stylesheet').setAttribute('href', "../css/styles-" + optionValue + ".css");
+  return optionValue;
 }
